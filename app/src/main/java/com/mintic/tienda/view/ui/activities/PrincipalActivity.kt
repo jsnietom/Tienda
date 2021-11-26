7f463ba92223bbc3322c28c12e1ed9aab9a68ca4
sebastian <jotapunk83@gmail.com>
2021-11-26 18:55:05 -0500

    actualizado

--- app/src/main/java/com/mintic/tienda/view/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/mintic/tienda/view/ui/activities/PrincipalActivity.kt
@@ -1,12 +1,18 @@
 package com.mintic.tienda.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mintic.tienda.R
+import kotlinx.android.synthetic.main.activity_principal.*
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+        openButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
