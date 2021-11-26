7b2201b9d415cb4bf38577e46e60f9ec1f2b4257
sebastian <jotapunk83@gmail.com>
2021-11-25 20:18:04 -0500

    Splash Screen

---
+++ app/src/main/java/com/mintic/tienda/view/ui/activities/PrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.mintic.tienda.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mintic.tienda.R
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
