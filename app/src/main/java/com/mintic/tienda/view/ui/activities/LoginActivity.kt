7aedbcdf5c55853db3a645ab9b92348b89374123
sebastian <jotapunk83@gmail.com>
2021-11-26 19:55:42 -0500

    actualizado

--- app/src/main/java/com/mintic/tienda/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/mintic/tienda/view/ui/activities/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.mintic.tienda.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mintic.tienda.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_principal.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        registro.setOnClickListener{
+            val intent = Intent(this, RegistroUsuarioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
