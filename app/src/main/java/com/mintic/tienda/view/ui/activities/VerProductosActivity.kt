5ff235db594a27e44ffa7f9398c18c3ab2696e57
sebastian <jotapunk83@gmail.com>
2021-11-29 14:24:56 -0500

    avance3

---
+++ app/src/main/java/com/mintic/tienda/view/ui/activities/VerProductosActivity.kt
@@ -0,0 +1,12 @@
+package com.mintic.tienda.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mintic.tienda.R
+
+class VerProductosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_productos)
+    }
+}
