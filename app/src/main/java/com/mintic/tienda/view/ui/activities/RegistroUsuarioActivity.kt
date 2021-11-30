6a1eccd99015147c7a5235dc338c85a4b7d03357
sebastian <jotapunk83@gmail.com>
2021-11-29 21:58:28 -0500

    Splash Screen

--- app/src/main/java/com/mintic/tienda/view/ui/activities/RegistroUsuarioActivity.kt
+++ app/src/main/java/com/mintic/tienda/view/ui/activities/RegistroUsuarioActivity.kt
@@ -1,12 +1,19 @@
 package com.mintic.tienda.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mintic.tienda.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registro_usuario.*
 
 class RegistroUsuarioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
+        registro_usuario.setOnClickListener{
+            val intent = Intent(this, VerProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
